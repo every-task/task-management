52e3185ae2015b4cd6aac424ea9c685a7fe1df86
ktk8916 <ktk8916@naver.com>
2023-10-24 12:10:51 +0900

    feat: QUESTION 토픽 추가

--- src/main/java/com/playdata/kafka/config/TopicConfig.java
+++ src/main/java/com/playdata/kafka/config/TopicConfig.java
@@ -9,11 +9,20 @@ import org.springframework.kafka.config.TopicBuilder;
 public class TopicConfig {
 
     public static final String SUCCESS_STORY = "successStory";
+    public static final String QUESTION = "question";
 
     @Bean
     public NewTopic topicSuccessStory(){
         return TopicBuilder
                 .name(SUCCESS_STORY)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+    @Bean
+    public NewTopic topicQuestion(){
+        return TopicBuilder
+                .name(QUESTION)
                 .partitions(1)
                 .replicas(1)
                 .build();
