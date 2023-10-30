48e4fddf673be05c3cc3c8c24f71989d5a197e7c
ktk8916 <ktk8916@naver.com>
2023-10-30 14:21:18 +0900

    feat: 답변 반환 작업을 objectMapper로 변경

--- src/main/java/com/playdata/client/chatgpt/service/ChatGptServiceImpl.java
+++ src/main/java/com/playdata/client/chatgpt/service/ChatGptServiceImpl.java
@@ -1,42 +1,44 @@
 package com.playdata.client.chatgpt.service;
 
 
-import com.playdata.client.chatgpt.request.GptCompletionRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.playdata.client.chatgpt.config.GptCompletion;
 import com.playdata.client.chatgpt.response.GptCompletionResponse;
 import com.theokanning.openai.completion.CompletionResult;
 import com.theokanning.openai.service.OpenAiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ChatGptServiceImpl implements ChatGptService {
     private final OpenAiService openAiService;
+    private final ObjectMapper objectMapper;
 
     @Override
     public List<String> parseContent(String content) {
-        String completion = completion(content);
-        List<String> word = extractWordsFromResponse(completion);
-        // 쉼표로 구분해서 리스트로 만들어서 반환
-        return word;
-
+        String completion = completion(content).trim();
+        return extractWordsFromResponse(completion);
     }
 
     public String completion(String prompt) {
-        CompletionResult result = openAiService.createCompletion(GptCompletionRequest.fromPrompt(prompt));
+        CompletionResult result = openAiService.createCompletion(GptCompletion.fromPrompt(prompt));
         GptCompletionResponse response = GptCompletionResponse.of(result);
 
         return response.getMessages().stream()
                 .map(GptCompletionResponse.Message::getText)
                 .findFirst()
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(()->new RuntimeException("커스텀 예외로 바꿔야 함"));
     }
 
     private List<String> extractWordsFromResponse(String response) {
-        String[] wordsArray = response.replaceAll("[\"\\[\\]]", "").split(", ");
-        return Arrays.asList(wordsArray);
+        try {
+            return objectMapper.readValue(response, List.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("커스텀 예외로 바꿔야 함");
+        }
     }
 }
