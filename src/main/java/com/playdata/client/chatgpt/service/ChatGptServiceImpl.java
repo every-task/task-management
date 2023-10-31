f29273ecf2fd33271a02a17262806e648969bef2
ktk8916 <ktk8916@naver.com>
2023-10-31 16:52:52 +0900

    feat: gpt 요청 실패시 재시도 추가

--- src/main/java/com/playdata/client/chatgpt/service/ChatGptServiceImpl.java
+++ src/main/java/com/playdata/client/chatgpt/service/ChatGptServiceImpl.java
@@ -8,6 +8,8 @@ import com.playdata.client.chatgpt.response.GptCompletionResponse;
 import com.theokanning.openai.completion.CompletionResult;
 import com.theokanning.openai.service.OpenAiService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,12 +21,13 @@ public class ChatGptServiceImpl implements ChatGptService {
     private final ObjectMapper objectMapper;
 
     @Override
+    @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 1000L))
     public List<String> parseContent(String content) {
         String completion = completion(content).trim();
         return extractWordsFromResponse(completion);
     }
 
-    public String completion(String prompt) {
+    private String completion(String prompt) {
         CompletionResult result = openAiService.createCompletion(GptCompletion.fromPrompt(prompt));
         GptCompletionResponse response = GptCompletionResponse.of(result);
 
