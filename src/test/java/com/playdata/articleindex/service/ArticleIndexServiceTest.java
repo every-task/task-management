44804b84e371508e84e0ef20d269a1e7819c13f4
ktk8916 <ktk8916@naver.com>
2023-11-03 15:03:06 +0900

    refactor: jpa 제거

--- src/test/java/com/playdata/articleindex/service/ArticleIndexServiceTest.java
+++ src/test/java/com/playdata/articleindex/service/ArticleIndexServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Set;
@@ -16,7 +15,6 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Transactional
 @ActiveProfiles("test")
 class ArticleIndexServiceTest {
 
