6063a04d1fbc01a9e8cbd6dc81c521e956ea0237
ktk8916 <ktk8916@naver.com>
2023-11-06 12:25:42 +0900

    test: 내장 mongo db 스프링 3 버전으로 변경

--- src/test/java/com/playdata/domain/articleindex/repository/ArticleIndexRepositoryTest.java
+++ src/test/java/com/playdata/domain/articleindex/repository/ArticleIndexRepositoryTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
+@ActiveProfiles("test")
 class ArticleIndexRepositoryTest {
 
     @Autowired
