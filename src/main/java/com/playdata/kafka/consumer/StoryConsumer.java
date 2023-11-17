41f8b696899ac5c73c9d7d027a0d39fc40aa4f33
ktk8916 <ktk8916@naver.com>
2023-11-17 10:26:33 +0900

    feat: kafka action 추가

--- src/main/java/com/playdata/kafka/consumer/StoryConsumer.java
+++ src/main/java/com/playdata/kafka/consumer/StoryConsumer.java
@@ -1,5 +1,6 @@
 package com.playdata.kafka.consumer;
 
+import com.playdata.kafka.config.KafkaData;
 import com.playdata.kafka.config.TopicConfig;
 import com.playdata.kafka.dto.ArticleKafkaData;
 import com.playdata.task.service.TaskService;
@@ -16,8 +17,9 @@ public class StoryConsumer {
     private final TaskService taskService;
 
     @KafkaListener(topics = TopicConfig.STORY)
-    public void listen(ArticleKafkaData data){
-        log.info("Received Story Id : {} Topic : {}", data.id(), TopicConfig.STORY);
-        taskService.taskRegister(data);
+    public void listen(KafkaData<ArticleKafkaData> data){
+        ArticleKafkaData articleKafkaData = data.data();
+        log.info("Received Story Id : {} Topic : {}", articleKafkaData.id(), TopicConfig.STORY);
+        taskService.taskRegister(articleKafkaData);
     }
 }
